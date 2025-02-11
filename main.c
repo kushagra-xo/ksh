81bd8ef63df74098bff72b90833aa7863e93fa60
Kushagra Jain <volodarolathana@tutanota.com>
2025-02-11 21:33:51 +0530

    feat(kshReadLine): getline

--- main.c
+++ main.c
@@ -1,42 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define KSH_RL_BUFSIZE 1024
-
 char *kshReadLine(void){
-    int bufsize = KSH_RL_BUFSIZE;
-    int position = 0;
-    char* buffer = malloc(sizeof(char) * bufsize);
-    int c;
-
-    if(!buffer){
-        printf("ksh: \nerror: memory allocation failure");
-        exit(EXIT_FAILURE);
-    }
-
-    while (1)
-    {
-        c = getchar();
-        if(c == EOF || c == "\n"){
-            buffer[position] = "\0";
-            return buffer;
-        }
+    char *line = NULL;
+    __ssize_t bufsize = 0;
+    if(getline(&line, &bufsize, stdin) == -1){
+        if(feof(stdin))
+            exit(EXIT_SUCCESS);
         else{
-            buffer[position] = c;
+            perror("readline");
+            exit(EXIT_FAILURE);
         }
-        position++;
-        if (position >= bufsize)
-        {
-            bufsize += KSH_RL_BUFSIZE;
-            buffer = realloc(buffer, bufsize);
-             if(!buffer){
-                printf("ksh: \nerror: memory allocation failure");
-                exit(EXIT_FAILURE);
-            }
-        }
-        
     }
-    
+    return line;
 }
 
 void kshLoop(void){
